102f3a3d17e8057f21ffe04c1e231258313e7f19
RadheRavi3021 <radheravi01@gmail.com>
2024-05-21 13:22:35 +0530

    git username changed

--- src/app/_model/socialLinks.ts
+++ src/app/_model/socialLinks.ts
@@ -1,7 +1,7 @@
 export class socialLinks{
     static readonly INSTAGRAM = new socialLinks ('https://www.instagram.com/ravi_radhey_02/');
     static readonly LINKEDIN = new socialLinks ('https://www.linkedin.com/in/radhe-ravi/');
-    static readonly GITHUB = new socialLinks ('https://www.github.com/RadheRavi3021');
+    static readonly GITHUB = new socialLinks ('https://github.com/radhe-ravi');
     static readonly LEETCODE = new socialLinks('https://www.leetcode.com/radheravi01');
     static readonly EMAIL = new socialLinks ('radheravi01@gmail.com');
     static readonly PHONE = new socialLinks ('+91 7250405391');
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -48,7 +48,7 @@ export class ProjectsComponent {
       name: "Simple Calculator",
       summary: "The calculator is a simple and easy-to-use calculator that can be used for basic calculations. The calculator is built using Angular and TypeScript, which makes it easy to maintain and extend. The calculator can be used in any web application that requires a simple calculator.",
       description: " something",
-      projectLink: "https://github.com/RadheRavi3021/simpleCalculator",
+      projectLink: "https://github.com/radhe-ravi/simpleCalculator",
       tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
       picture: "../../assets/images/SimpleCalculator.png"
     },
@@ -57,7 +57,7 @@ export class ProjectsComponent {
       name: "EDU-CONNECT",
       summary: "Edu Connect is a learning portal built using Spring Boot. The portal provides a platform for students and teachers to connect and learn. The portal has features such as course creation, course management, student management, and user authentication.",
       description: " something",
-      projectLink: "https://github.com/RadheRavi3021/EDUCONNECT",
+      projectLink: "https://github.com/radhe-ravi/EDUCONNECT",
       tags: [Tag.JAVA, Tag.SPRINGBOOT],
       picture: '../../assets/images/edu.png'
     },
@@ -66,7 +66,7 @@ export class ProjectsComponent {
       name: "Connect Four",
       summary: "Connect Four is a two-player game implemented using JavaFX and Java. Players take turns dropping colored discs, The goal is to connect four discs in a row before the opponent does. The game can be won horizontally, vertically, or diagonally.",
       description: " something",
-      projectLink: "https://github.com/RadheRavi3021/ConnectFour",
+      projectLink: "https://github.com/radhe-ravi/ConnectFour",
       tags: [Tag.JAVA, Tag.JAVAFX],
       picture: "../../assets/images/connect.png"
     },
@@ -75,7 +75,7 @@ export class ProjectsComponent {
       name: "Portfolio",
       summary: "A portfolio is a website that showcases a person's skills, experiences, and achievements. A portfolio can be built using Angular and TypeScript.",
       description: " something",
-      projectLink: "https://github.com/RadheRavi3021/portfolioInAngular",
+      projectLink: "https://github.com/radhe-ravi/portfolioInAngular",
       tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
       picture: "../../assets/images/port.png"
     },
@@ -85,7 +85,7 @@ export class ProjectsComponent {
       name: "Newspaper Agency Manager",
       summary: ` Newspaper Agency Manager Tech is a Java-based desktop application that helps manage the day-to-day operations of a newspaper agency. The system is designed to automate various tasks, such as managing subscriptions, tracking payments, and generating reports.`,
       description: " something",
-      projectLink: "https://github.com/RadheRavi3021/NEWSPAPER_AGENCY_MANAGER",
+      projectLink: "https://github.com/radhe-ravi/NEWSPAPER_AGENCY_MANAGER",
       tags: [Tag.JAVA, Tag.JAVAFX],
       picture: "../../assets/images/news.png"
     },
@@ -94,7 +94,7 @@ export class ProjectsComponent {
       name: "Authentication",
       summary: "Designd a secure and scalable authentication microservice for a portfolio website project using Spring Boot and Java, incorporating best practices for user authentication and authorization.",
       description: " something",
-      projectLink: "https://github.com/RadheRavi3021/portfolioInAngular",
+      projectLink: "https://github.com/radhe-ravi/portfolioInAngular",
       tags: [Tag.JAVA, Tag.SPRINGBOOT],
       picture: "../../assets/images/auth.png"
     },
